495cd080e80e8734525e0c3f90dc7e1eaf177c63
Chris van Meer <chris@van-meer.net>
2022-07-02 07:49:28 +0200

    Modified the hostname object to the name object.

--- terraform/inventory.tf
+++ terraform/inventory.tf
@@ -3,11 +3,11 @@ resource "local_file" "ansible_inventory" {
     {
       google = tomap({
         for instance in google_compute_instance.google_vault :
-        instance.hostname => instance.network_interface.0.access_config.0.nat_ip
+        instance.name => instance.network_interface.0.access_config.0.nat_ip
       })
       azure = tomap({
         for instance in azurerm_linux_virtual_machine.azure_vault :
-        instance.name => instance.public_ip
+        instance.name => instance.public_ip_address
       })
       aws = tomap({
         for instance in aws_instance.aws_vault :
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,7 @@
 output "google_vault_adddresses" {
   value = formatlist(
     "%s: %s",
-    google_compute_instance.google_vault[*].hostname,
+    google_compute_instance.google_vault[*].name,
     google_compute_instance.google_vault[*].network_interface.0.access_config.0.nat_ip
   )
 }
